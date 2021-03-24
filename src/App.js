7e06f0a1576a14d9e1e3481dcfd4a2879403a989
thenerdie <stirmanjr@gmail.coom>
2021-03-23 21:34:11 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import data from "./Data.json"
+
+import { Line, LineChart, XAxis, YAxis } from 'recharts'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TSI for collected data samples</h1>
+        <LineChart width={1500} height={510} data={data}>
+          <Line type="monotone" dataKey="TSI" stroke="#dff705" />
+          <XAxis dataKey="Date" interval={7} padding={{ left: 50 }} label = {{ value: 'Date', position: 'center', dy: -40}}/>
+          <YAxis label={{ value: 'Measured TSI', angle: -90, position: 'center', dx: -25}}/>
+        </LineChart>
       </header>
     </div>
   );
